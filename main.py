12e64528357e47e6728e407be4f0128d5e653f7d
oldhill <oldhillcode@gmail.com>
2014-11-30 19:43:46 -0800

    Hello world rickshaw example

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
-""" dun dun dun
+"""Recommends SoundCloud artists based on who an input artist follows
 """
 
+import os
+
+import jinja2
 import webapp2
 
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+  loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+  extensions=['jinja2.ext.autoescape'],
+  autoescape=True
+)
+
+
 class MainHandler(webapp2.RequestHandler):
+  """ Index
+  TODO: move this out of main file...
+  """
   def get(self):
-    self.response.write('wat')
+    template = JINJA_ENVIRONMENT.get_template('templates/index.html')
+    self.response.write(template.render())
+
 
 app = webapp2.WSGIApplication([
-  ('/', MainHandler)
+  ('/', MainHandler),
 ], debug=True)
